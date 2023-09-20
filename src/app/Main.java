f57b9c543e69afe7369d94e2dbcf20fdd0c48d5f
BieliaievVlad <rostik.khokhlov@gmail.com>
2023-09-20 12:56:37 +0300

    Update app

--- src/app/Main.java
+++ src/app/Main.java
@@ -18,36 +18,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        product = "Phone";
-        price = 2775.99;
-        measure = "pcs";
-
-        showProduct();
         getData();
-        calcPayment();
-        calcDiscont();
-        calcDiscAmount();
-        calcDiscPayment();
+        showProduct();
+        calcPayment(price, amount);
+        calcDiscont(payment);
+        calcDiscAmount(payment, discount);
+        calcDiscPayment(payment, discAmount);
         getOutput(payment, discAmount, discPayment);
     }
 
     private static void showProduct() {
-        System.out.printf("Product name: %s%nPrice, UAH: %.2f/%S", product, price, measure);
+        System.out.print("\n-------------------");
+        System.out.printf("%nProduct name: %s%nPrice, UAH: %.2f/%S", product, price, measure);
     }
 
     private static void getData() {
         sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
-        System.out.print("\nEnter amount of product: ");
+        System.out.print("Enter product name: ");
+        product = sc.nextLine();
+        System.out.print("Enter product measure value: ");
+        measure = sc.nextLine();
+        System.out.print("Enter product price value, UAH: ");
+        price = sc.nextDouble();
+        System.out.print("Enter amount of product: ");
         amount = sc.nextInt();
+
         sc.close();
     }
 
-    private static void calcPayment() {
-        payment = price * amount;
+     private static double calcPayment(double price, int amount) {
+        return payment = price * amount;
     }
 
-    private static void calcDiscont() {
+    private static double calcDiscont(double payment) {
         if (payment > 0 & payment <= 5000) {
             discount = 5;
         } else if (payment > 5000.01 & payment <= 10000) {
@@ -55,20 +59,22 @@ public class Main {
         } else if (payment > 10000.01) {
             discount = 15;
         }
+        return discount;
     }
 
-    private static void calcDiscAmount() {
-        discAmount = payment * discount / 100;
+    private static double calcDiscAmount(double payment, double discount) {
+        return discAmount = payment * discount / 100;
     }
 
-    private static void calcDiscPayment() {
-        discPayment = payment - discAmount;
+    private static double calcDiscPayment(double payment, double discAmount) {
+        return discPayment = payment - discAmount;
     }
 
-    private static void getOutput(double payment, double discAmount, double discPayment) {
-        System.out.printf("Payment, UAH: %.2f%n" +
+    private static void getOutput(double pay, double discAmount, double discPay) {
+
+        System.out.printf("%nPayment, UAH: %.2f%n" +
                 "Discount, UAH: %.2f%n" +
-                "Total, UAH: %.2f", payment, discAmount, discPayment);
+                "Total, UAH: %.2f", pay, discAmount, discPay);
     }
 
 
